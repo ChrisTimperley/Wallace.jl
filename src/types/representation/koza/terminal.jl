53a4c018a133b72344ac18b75d16dab653f3c8ee
ChrisTimperley <christimperley@googlemail.com>
2015-04-20 23:00:58 +0100

    Re-added terminal.jl; strangely disappeared during migration.

---
+++ src/types/representation/koza/terminal.jl
@@ -0,0 +1,36 @@
+load("input", dirname(@__FILE__))
+load("node", dirname(@__FILE__))
+
+abstract KozaTerminal <: KozaNode
+function ComposeKozaTerminal(inputs::Vector{KozaInput}, def::String)
+  label = def[1:Base.search(def, ':')-1]
+  ty = def[Base.last(Base.search(def, "::"))+1:end]
+  
+  # Compose the unique type for this non-terminal.
+  src = "type <: KozaTerminal
+    parent::KozaParent
+    constructor() = new()
+    constructor(p::KozaParent) = new(p)
+  end"
+  t = anonymous_type(Wallace, src)
+
+  # Compose the label function.
+  eval(Wallace, parse("label(::$(t)) = $(label)"))
+
+  # Compose the "fresh" function.
+  eval(Wallace, parse("fresh(::$(t), p::KozaParent) = $(t)(p)"))
+
+  # Compose the execution function.
+  src = join([["::$(t)"], ["$(i.label)::$(i.ty)" for i in inputs]], ",")
+  src = "execute($(src)) = $(label)"
+  println(src)
+  eval(Wallace, parse(src))
+
+  return t
+end
+
+clone{T <: KozaTerminal}(n::T, p::KozaParent) = T(p) 
+isterminal(::KozaTerminal) = true
+num_nodes(n::KozaTerminal) = 1
+height(n::KozaTerminal) = 1
+describe(n::KozaTerminal) = label(n)
