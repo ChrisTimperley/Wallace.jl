4088dc14383170155a5a9d285f61d3ad7b26b109
ChrisTimperley <christimperley@googlemail.com>
2015-04-21 23:59:25 +0100

    Added examples function for listing example problems.

--- src/kernel/example.jl
+++ src/kernel/example.jl
@@ -1,6 +1,9 @@
+# Store the path to the example scripts.
+EXAMPLES_PATH = joinpath(dirname(@__FILE__), "../../examples")
+
 # Composes an instance of a given example problem.
 function example(name::String)
-  path = joinpath(dirname(@__FILE__), "../../examples/$(name).cfg")
+  path = joinpath(EXAMPLES_PATH, "$(name).cfg")
 
   # Throw an error if the requested example doesn't exist.
   if !isfile(path)
@@ -10,3 +13,9 @@ function example(name::String)
   # Otherwise load and compose the configuration file for the given example.
   return compose(path)
 end
+export example
+
+# Returns a list of all available example problems.
+examples() = map!(f -> f[1:end-4], 
+  filter!(f -> isfile(joinpath(EXAMPLES_PATH, f)) && endswith(f, ".cfg"), readdir(EXAMPLES_PATH)))
+export examples
