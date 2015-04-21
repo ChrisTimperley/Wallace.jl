dacbecd9ea493a8a33cdb82a311862ebca1eb9d1
ChrisTimperley <christimperley@googlemail.com>
2015-04-22 00:40:45 +0100

    Fixed example function.

--- src/kernel/example.jl
+++ src/kernel/example.jl
@@ -7,7 +7,7 @@ function example(name::String)
 
   # Throw an error if the requested example doesn't exist.
   if !isfile(path)
-    fail("Failed to locate specified example within examples directory: $(name).") 
+    error("Failed to locate specified example within examples directory: $(name).") 
   end
   
   # Otherwise load and compose the configuration file for the given example.
