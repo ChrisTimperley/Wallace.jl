6b08fb7fdb8aea8abeb8ca3f57783b9bba565007
ChrisTimperley <christimperley@googlemail.com>
2015-05-11 23:13:40 +0100

    Fixed Base.parse in DefaultInitializer.

--- src/types/initializer/default.jl
+++ src/types/initializer/default.jl
@@ -6,7 +6,7 @@ end
 
 function initialize!{I <: Individual}(i::DefaultInitializer, d::Deme{I})
   rep = d.species.genotype.representation
-  getter = eval(parse("i -> i.$(d.species.genotype.label)"))
+  getter = eval(Base.parse("i -> i.$(d.species.genotype.label)"))
   for ind in d.members
     getter(ind).value = rand(rep)
   end
