fdf6695ddb33dabf5633450a2cee04a70f49b5c2
ChrisTimperley <christimperley@googlemail.com>
2015-05-27 18:43:03 +0100

    Begun work on fitness schemes.

--- src/types/fitness.jl
+++ src/types/fitness.jl
@@ -1,3 +1,14 @@
 abstract Fitness
 
+abstract FitnessScheme
+
+sort(s::FitnessScheme, x::Individual, y::Individual) =
+
+
+compare(s::FitnessScheme, x::Individual, y::Individual) =
+  compare(s, x.fitness, y.fitness)
+
+best(s::FitnessScheme, x::Individual, y::Individual) =
+  compare(s, x, y) == 1 ? y : x
+
 Wallace.register(joinpath(dirname(@__FILE__), "fitness.manifest.yml"))
