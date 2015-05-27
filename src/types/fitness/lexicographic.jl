8e0837a8115999ec6c38fbc620e79b9ec0030e08
ChrisTimperley <christimperley@googlemail.com>
2015-05-27 16:15:51 +0100

    Implemented lexicographic fitness scheme.

--- src/types/fitness/lexicographic.jl
+++ src/types/fitness/lexicographic.jl
@@ -1,11 +1,12 @@
 type LexicographicFitnessScheme <: FitnessScheme
   randomised::Bool
-  objectives::Vector{Integer} 
+  preferences::Vector{Integer} 
+
+  LexicographicFitnessScheme(r::Bool, o::Vector{Integer}) = new(r, o)
 end
 
 function compare{T}(s::LexicographicFitnessScheme, x::Vector{T}, y::Vector{T})
-  # If randomised, shuffle the objectives.
-  for i in s.objectives
+  for i in (s.randomised ? shuffle(s.objectives) : s.objectives)
     if x[i] != y[i]
       return y[i] > x[i] ? 1 : -1
     end
