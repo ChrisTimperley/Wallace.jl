1b6b6796e9d5286b71cf81b666c84e233d4eadba
ChrisTimperley <christimperley@googlemail.com>
2015-05-27 21:37:48 +0100

    Changed references to Numeric to Number.

--- src/types/fitness/scalar/scalar.jl
+++ src/types/fitness/scalar/scalar.jl
@@ -1,6 +1,6 @@
 load("../../fitness", dirname(@__FILE__))
 
-type ScalarFitnessScheme{T <: Numeric} <: FitnessScheme
+type ScalarFitnessScheme{T <: Number} <: FitnessScheme
   maximise::Bool
 
   ScalarFitnessScheme(m::Bool) = new(m)
@@ -8,7 +8,7 @@ end
 
 uses{T}(s::ScalarFitnessScheme{T}) = T
 
-compare{T <: Numeric}(s::ScalarFitnessScheme{T}, x::T, y::T) =
+compare{T <: Number}(s::ScalarFitnessScheme{T}, x::T, y::T) =
   if x > y
     s.maximise ? -1 : 1
   elseif x < y
