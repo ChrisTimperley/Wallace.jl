d06d73c9ea592ae50cca4f2d57a302b569066079
ChrisTimperley <christimperley@googlemail.com>
2015-05-27 20:05:41 +0100

    Modified implementation of fitness schemes.

--- src/types/individual.jl
+++ src/types/individual.jl
@@ -5,6 +5,24 @@ load("individual/stage.jl", dirname(@__FILE__))
 # The base type used by all individuals.
 abstract Individual
 
+# Sorts a list of individuals in descending order of fitness,
+# from best to worst.
+sort{I <: Individual}(s::FitnessScheme, l::Vector{I}) =
+  sort(l, lt = (x, y) -> isbetter(s, x, y)) 
+
+# Sorts a list of individuals in descending order of fitness,
+# from best to worst, in place.
+sort!{I <: Individual}(s::FitnessScheme, l::Vector{I}) =
+  sort!(l, lt = (x, y) -> isbetter(s, x, y))
+
+# Compares an individual X to an individual Y to determine whether X is
+# an improvement upon Y.
+isbetter(s::FitnessScheme, x::Individual, y::Individual) =
+  compare(s, x, y) == -1
+
+compare(s::FitnessScheme, x::Individual, y::Individual) =
+  !x.evaluated ? 1 : compare(s, x.fitness, y.fitness)
+
 Base.isless(x::Individual, y::Individual) =
   !x.evaluated || (y.evaluated && x.fitness < y.fitness)
 Base.isequal(x::Individual, y::Individual) = 
