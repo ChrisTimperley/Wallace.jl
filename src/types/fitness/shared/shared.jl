825c081940028245d748a60bfa910db8613c2080
ChrisTimperley <christimperley@googlemail.com>
2015-06-04 00:24:02 +0100

    Implemented fitness, uses, and compare for shared fitness.

--- src/types/fitness/shared/shared.jl
+++ src/types/fitness/shared/shared.jl
@@ -9,22 +9,34 @@ type FitnessSharingScheme <: FitnessScheme
   base::FitnessScheme
   radius::Float
   alpha::Float
+  maximise::Bool
   dist::Distance
 
-  FitnessSharingScheme(b::FitnessScheme, r::Float, a::Float, d::Distance) =
-    new(b, r, a, d)
+  FitnessSharingScheme(b::FitnessScheme, r::Float, a::Float, m::Bool, d::Distance) =
+    new(b, r, a, m, d)
 end
 
-# Sharing function.
-sh(s::FitnessSharingScheme, d::Float) =
-  d <= s.radius ? 1.0 - (d/s.radius) ^ s.alpha : 0.0
+uses{T}(s::FitnessSharingScheme{T}) = SharedFitness{T}
 
-function scale!{I <: Individual}(s::FitnessSharingScheme, inds::Vector{I})
+fitness{T}(s::FitnessSharingScheme{T}, args...) =
+  fitness(s.base, args...)
+
+sh(s::FitnessSharingScheme, d::Float) =
+  d < s.radius ? 1.0 - (d/s.radius) ^ s.alpha : 0.0
+
+scale!{I <: Individual}(s::FitnessSharingScheme, inds::Vector{I}) =
   for i1 in inds
-    # Vicinity parameter
     i1.fitness.shared = score(i1.fitness.base) /
       sum(i2 -> sh(s, distance(s.distance, i1, i2)), inds)
   end
-end
+
+compare(s::FitnessSharingScheme, x::SharedFitness, y::SharedFitness) =
+  if x.shared > y.shared
+    s.maximise ? -1 : 1
+  elseif x.shared < y.shared
+    s.maximise ? 1 : -1
+  else
+    0
+  end
 
 register(joinpath(dirname(@__FILE__), "shared.manifest.yml"))
