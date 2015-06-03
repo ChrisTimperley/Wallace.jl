aa5d037ab570c5e1c268b6f7328512d78cf8b024
ChrisTimperley <christimperley@googlemail.com>
2015-06-03 13:49:26 +0100

    Implemented Euclidean distance calculation.

--- src/types/distance/euclidean/euclidean.jl
+++ src/types/distance/euclidean/euclidean.jl
@@ -1,2 +1,12 @@
 immutable EuclideanDistance <: Distance
 end
+
+distance(::EuclideanDistance, x::Number, y::Number) = abs(x - y)
+
+function distance{T <: Number}(::EuclideanDistance, x::Vector{T}, y::Vector{T})
+  d = zero(T)
+  for (a, b) in zip(x, y)
+    d += (a - b) ** 2
+  end
+  return d
+end
