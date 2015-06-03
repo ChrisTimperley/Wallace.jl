ea201756f3e09f7b4716017fb9046a3c52c6c956
ChrisTimperley <christimperley@googlemail.com>
2015-06-03 15:00:39 +0100

    Begun integration of Goldberg fitness scheme.

---
+++ src/types/fitness/goldberg/goldberg.jl
@@ -0,0 +1,28 @@
+type GoldbergFitnessScheme <: FitnessScheme
+  maximise::Vector{Bool}
+end
+
+function process!(s::GoldbergFitnessScheme, inds::Vector{Wrapper})
+  n = length(inds)
+  j = k = rank = 1
+
+  # Keep calculating each of the pareto fronts until all individuals have
+  # been handled.
+  while j <= n
+
+    # Check each remaining member for inclusion in the current pareto front.
+    # If the individual belongs to the pareto front then swap it with the 
+    # first unsorted individual.
+    for i in j:n
+      p1 = inds[i]
+      if all(p2 -> p1 == p2 || !dominates(p2.fitness.scores, p1.fitness.scores, s.maximise), inds[j:end])
+        p1.fitness.rank = rank
+        inds[k], inds[i] = inds[i], inds[k]
+        k += 1
+      end
+    end
+
+    j = k
+    rank += 1
+  end
+end
