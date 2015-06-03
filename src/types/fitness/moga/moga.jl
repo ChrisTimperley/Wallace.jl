146936a3c04dff6ec26f60a9ab31cf0e4b7b026a
ChrisTimperley <christimperley@googlemail.com>
2015-06-03 14:50:53 +0100

    Added MOGA fitness scheme.

---
+++ src/types/fitness/moga/moga.jl
@@ -0,0 +1,8 @@
+type MOGAFitnessScheme <: FitnessScheme
+  maximise::Vector{Bool}
+end
+
+process!{I <: Individual}(s::MOGAFitnessScheme, inds::Vector{I}) =
+  for p1 in inds
+    p1.fitness.rank = 1 + count(p2 -> dominates(p2.fitness.scores, p1.fitness.scores, s.maximise), inds)
+  end
