47e9269b44f1aa34850e028781b4d15de94b5688
ChrisTimperley <christimperley@googlemail.com>
2015-07-26 00:33:22 +0100

    Removed one max evaluator.

--- src/types/evaluator/one_max.jl
+++
@@ -1,9 +0,0 @@
-load("simple",            dirname(@__FILE__))
-load("../fitness/simple", dirname(@__FILE__))
-
-type OneMaxEvaluator <: SimpleEvaluator; end
-
-evaluate!(::OneMaxEvaluator, s::State, c::Individual) =
-  SimpleFitness{Int}(true, sum(c.genome))
-
-register(joinpath(dirname(@__FILE__), "one_max.manifest.yml"))
