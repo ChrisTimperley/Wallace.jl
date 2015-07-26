cba63a014bf42bd85b0093cb99e71dc7dd7833f0
ChrisTimperley <christimperley@googlemail.com>
2015-07-26 16:47:52 +0100

    Modified permutation representations.

--- src/types/representation/permutation/permutation.jl
+++ src/types/representation/permutation/permutation.jl
@@ -1,26 +1,13 @@
 load("../../representation", dirname(@__FILE__))
 
 immutable PermutationRepresentation{T} <: Representation
-  alphabet::T
+  alphabet::Vector{T}
   size::Integer
 
-  KozaTreeRepresentation(tree::Type,
-    min_d::Int,
-    max_d::Int,
-    t::Vector{KozaTerminal},
-    nt::Vector{KozaNonTerminal},
-    i::Vector{KozaInput},
-    b::KozaBuilder) =
-    new(tree, min_d, max_d, t, nt, i, b)
+  PermutationRepresentation(a::Vector{T}) = new(a, length(a))
 end
 
-Base.rand(r::KozaTreeRepresentation) = build(r.builder, r)
-chromosome(r::KozaTreeRepresentation) = r.tree
-
-sample_terminal(r::KozaTreeRepresentation, p::KozaParent) =
-  fresh(r.terminals[rand(1:end)], p)
-
-sample_non_terminal(r::KozaTreeRepresentation, p::KozaParent) =
-  fresh(r.non_terminals[rand(1:end)], p)
+Base.rand(r::PermutationRepresentation) = shuffle(r.alphabet) 
+chromosome{T}(r::PermutationRepresentation{T}) = Vector{T}
 
 register(joinpath(dirname(@__FILE__), "manifest.yml"))
