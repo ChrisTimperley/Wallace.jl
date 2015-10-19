05428693a0d09e0d097f148472d8da1dee25fee6
Chris Timperley <christimperley@googlemail.com>
2015-10-19 13:37:21 -0400

    More changes to composers.

--- src/kernel/composer.jl
+++ src/kernel/composer.jl
@@ -13,21 +13,22 @@ module Composer
   end
   function compose_with(refinements::Function, file::AbstractString)
     s = parse_file(file)
-    apply(refinements, [s])
+    refinements(s)
     return compose_as(s, s["type"])
   end
 
   # Composes a given specification file into an object, using a predetermined composer.
   compose_as(file::AbstractString, as::AbstractString) =
     compose_as(parse_file(file), as)
+
   function compose_as_with(refinements::Function, file::AbstractString, as::AbstractString)
     s = parse_file(file)
-    apply(refinements, [s])
+    refinements(s)
     return compose_as(s, as)
   end
 
   # Composes a given specification object (in the form of a JSON object)
   # into the object it describes.
   compose_as{X,Y}(s::Dict{X, Y}, as::AbstractString) =
-    composer(as)(*[s])
+    composer(as)(s)
 end
