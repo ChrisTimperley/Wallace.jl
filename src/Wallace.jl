86d9a3e070922459bfe06579f420ea0fedf20f61
Chris Timperley <christimperley@googlemail.com>
2015-10-20 19:39:50 -0400

    Removed all kernel code.

--- src/Wallace.jl
+++ src/Wallace.jl
@@ -1,38 +1,20 @@
 push!(LOAD_PATH, dirname(@__FILE__))
 
 module Wallace
-  export register, compose
-
-  # Load and import the base and kernel modules.
   include(joinpath(dirname(@__FILE__), "base/identity.jl"))
   include("base/float.jl")
   include("base/partition.jl")
   include("base/each.jl")
   include("base/Reflect.jl")
-  include("kernel/parser.jl")
-  include("kernel/registry.jl")
-  include("kernel/composer.jl")
-  include("kernel/example.jl")
-  include("kernel/help.jl")
 
   using .Each
   using .Reflect
   using .Partition
-  using .Registry
-  using .Parser
-  using .Composer
-  using .Help
   using DataStructures
   using StatsBase
 
-  using criterion
-  export criterion
-
-  using replacement
-  export replacement
-
-  using evaluator
-  export evaluator
- 
+  using criterion; export criterion
+  using replacement; export replacement
+  using evaluator; export evaluator
   using logger; export logger
 end
