622d56a39d6374d9299ac0aa0f4d689a598c4ab7
Chris Timperley <christimperley@googlemail.com>
2015-10-21 01:05:38 -0400

    Ported float vectors.

--- src/representation/float_vector.jl
+++ src/representation/float_vector.jl
@@ -1,5 +1,3 @@
-load("../representation", dirname(@__FILE__))
-
 type FloatVectorRepresentation <: Representation
   length::Int
   min::Float
@@ -10,11 +8,30 @@ type FloatVectorRepresentation <: Representation
     new(length, min, max, min:eps():max)
 end
 
+"""
+Float vectors are implemented as a fixed-length vector of floating point
+numbers.
+
+**Parameters:**
+* `length::Int`, the length of this vector. Defaults to 80, if no value is
+supplied.
+* `min::Float`, the minimum value that a component of this vector may assume.
+Defaults to 0.0, if no value is supplied.
+* `max::Float`, the maximum value that a component of this vector may assume.
+Defaults to 1.0, if no value is supplied.
+"""
+float_vector{S <: AbstractString}(args::Dict{S, Any}) =
+  float_vector( get(args, "length", 80),
+                get(args, "min", 0.0),
+                get(args, "max", 1.0))
+float_vector() = float_vector(80, 0.0, 1.0)
+float_vector(length::Int) = float_vector(length, 0.0, 1.0)
+float_vector(length::Int, min::Float, max::Float) =
+  FloatVectorRepresentation(length, min, max)
+
 minimum_value(r::FloatVectorRepresentation) = r.min
 maximum_value(r::FloatVectorRepresentation) = r.max
 codon_type(::FloatVectorRepresentation) = Float
 chromosome(r::FloatVectorRepresentation) = Vector{Float}
 describe(i::Vector{Float}) = "($(join(i, ", ")))"
 Base.rand(r::FloatVectorRepresentation) = rand(r.range, r.length)
-
-register(joinpath(dirname(@__FILE__), "float_vector.manifest.yml"))
