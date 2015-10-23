104265cbdcdbe2ed95c86fe8b7068351eff06d7a
Chris Timperley <christimperley@googlemail.com>
2015-10-22 20:26:22 -0400

    Modified Wallace module to use importall rather than using.

--- src/Wallace.jl
+++ src/Wallace.jl
@@ -8,22 +8,22 @@ module Wallace
   using utility
   using common
   using core
-  using distance; export distance
-  using fitness; export fitness
-  using individual
-  using representation; export representation
-  using selection; export selection
-  using variation; export variation
-  using crossover; export crossover
-  using mutation; export mutation
-  using breeder; export breeder
-  using _deme_
-  using population; export population
-  using initialiser; export initialiser
-  using state; export state
-  using criterion; export criterion
-  using replacement; export replacement
-  using evaluator; export evaluator
-  using logger; export logger
-  using algorithm; export algorithm
+  importall distance
+  importall fitness
+  importall individual
+  importall representation
+  importall selection
+  importall variation
+  importall crossover
+  importall mutation
+  importall breeder
+  importall _deme_
+  importall population
+  importall initialiser
+  importall state
+  importall criterion
+  importall replacement
+  importall evaluator
+  importall logger
+  importall algorithm
 end
