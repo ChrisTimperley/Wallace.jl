072668baf300863b6641ace3df704f5775ba661c
Chris Timperley <christimperley@googlemail.com>
2015-10-28 15:21:14 +0000

    Ported Koza FULL builder.

--- src/representation/koza/builder/full/full.jl
+++ src/representation/koza/builder/full/full.jl
@@ -1,12 +1,43 @@
-load("../../builder", dirname(@__FILE__))
-load("../../parent",  dirname(@__FILE__))
-load("../../../koza", dirname(@__FILE__))
-
+"""
+Implements the Koza FULL initialisation method.
+"""
 type KozaFullBuilder{T <: KozaTree} <: KozaBuilder
+  """
+  The minimum depth of trees created according to this method.
+  """
   min_depth::Int
+
+  """
+  The maximum depth of trees created according to this method.
+  """
   max_depth::Int
+
+  KozaFullBuilder() = new(1, 8)
+  KozaFullBuilder(min::Int, max::Int) = new(min, max)
 end
 
+"""
+Implements the Koza FULL initialisation method, where some `d` is selected
+between a specified minimum and maximum depth, and used to generate a full tree
+with a depth equal to `d`.
+
+**Properties:**
+
+* `min::Int`, the minimum tree depth.
+* `max::Int`, the maximum tree depth.
+"""
+full() = KozaFullBuilder()
+full(min::Int, max::Int) = KozaFullBuilder(min, max)
+function full(f::Function)
+  def = KozaFullBuilder()
+  f(def)
+  def
+end
+
+"""
+Constructs a Koza Tree belonging to a given representation using the FULL
+method, according to a set of provided specifics.
+"""
 function build{T}(b::KozaFullBuilder{T}, r::KozaTreeRepresentation)
   t = T()
   t.root = build(KozaFullBuilder, r, t, 1, rand(b.min_depth:b.max_depth))
@@ -28,5 +59,3 @@ build(::Type{KozaFullBuilder},
     end
     return n
   end
-
-register(joinpath(dirname(@__FILE__), "manifest.yml"))
