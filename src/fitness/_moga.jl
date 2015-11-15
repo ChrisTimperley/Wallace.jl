25062b6b3e590d54715af82366ab9160c8eba842
ChrisTimperley <christimperley@googlemail.com>
2015-11-15 21:56:11 +0000

    Ported MOGA fitness scheme.

--- src/fitness/_moga.jl
+++ src/fitness/_moga.jl
@@ -8,7 +8,7 @@ TODO: Describe MOGA.
 moga(s::Dict{Any, Any}) =
   MOGAFitnessScheme{eval(Base.parse(Base.get(s, "of", "Float")))}(s["maximise"])
 
-scale!{I <: Individual}(s::MOGAFitnessScheme, inds::Vector{I}) =
-  for p1 in inds
-    p1.fitness.rank = 1 + count(p2 -> dominates(p2.fitness.scores, p1.fitness.scores, s.maximise), inds)
+scale!{F}(s::MOGAFitnessScheme, inds::Vector{Tuple{Int, F}}) =
+  for (p1_id, p1_f) in inds
+    p1_f.rank = 1 + count((p2_id, p2_f) -> dominates(p2_f.scores, p1_f.scores, s.maximise), inds)
   end
