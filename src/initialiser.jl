b03ea474ad1895e492338af7f8f302cc2c7feab2
ChrisTimperley <christimperley@googlemail.com>
2015-11-15 22:29:51 +0000

    Ported initialisers.

--- src/initialiser.jl
+++ src/initialiser.jl
@@ -10,29 +10,25 @@ abstract Initialiser
 """
 Initialises all members within a given population.
 """
-initialise!(i::Initialiser, p::Population) = for d in p.demes
-  initialise!(i, d)
+initialise!(init::Initialiser, p::Population) = for d in p.demes
+  initialise!(init, d)
 end
 
 """
 Initialises all members within a given deme.
 """
-initialise!{I <: Individual}(i::Initialiser, d::Deme{I}) = for ind in d.members
-  initialise!(i, ind)
+initialise!(init::Initialiser, d::Deme) = for i in 1:d.capacity
+  initialise!(init, i)
 end
 
 """
 The default initialiser used by all evolutionary algorithms.
 """
-type DefaultInitialiser <: Initialiser
-end
+type DefaultInitialiser <: Initialiser; end
 
-function initialise!{I <: Individual}(i::DefaultInitialiser, d::Deme{I})
+function initialise!(i::DefaultInitialiser, d::Deme)
   rep = d.species.genotype.representation
-  getter = eval(Base.parse("i -> i.$(d.species.genotype.label)")) # TODO: Not a big fan.
-  for ind in d.members
-    getter(ind).value = rand(rep)
-  end
+  d.members[d.species.genotype.label] = [rand(rep) for i in 1:d.capacity]
 end
 
 end
