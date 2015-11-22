10102b8781946d63f8b805fc1f10d5eec6bcc5e6
ChrisTimperley <christimperley@googlemail.com>
2015-11-22 17:09:37 +0000

    Added indivduals_to_csv method.

--- src/logger/evo_analyser.jl
+++ src/logger/evo_analyser.jl
@@ -11,6 +11,50 @@ function log!()
 end
 
 """
+Converts a provided collection of individuals into a CSV string, where each
+individual is represented by a record within the string.
+
+**Parameters:**
+
+* ``ic::IndividualCollection``, the collection of individuals.
+* ``fs::FitnessScheme``, the fitness scheme used by the individuals belonging
+  to the collection.
+* ``prefix::UTF8String``, the prefix that should be added to each CSV
+  record. Used to provide generation and deme numbers. If any information
+  is provided in the prefix, it MUST be suffixed by a semi-colon.
+
+**Returns:**
+
+A CSV string, in the form of a UTF-8 string.
+"""
+
+function individuals_to_csv(
+  ic::IndividualCollection,
+  fs::FitnessScheme,
+  prefix::UTF8String
+)::UTF8String
+  # Initialise the CSV row for each individual.
+  num = length(ic)
+  serialised = UTF8String["$(prefix) $(i)" for i in 1:num]
+
+  # Record the developmental stage for each individual.
+  for rep in keys(ic.stages)
+    for (i, stage) in enumerate(ic.stages[rep])
+      serialised[i] = serialised[i] * "; $(to_s(stage))"
+    end
+  end
+
+  # Add the fintess values for each individual to the JSON objects,
+  # along with their evaluated flag.
+  for (i, csv) in enumerate(serialised)
+    csv = csv * "; true; " * to_s(fs, ic.fitnesses[i])
+  end
+
+  # Glue together the individual CSV entries.
+  return join(serialised, "\n")
+end
+
+"""
 Converts a provided collection of individuals into a list of JSON descriptions.
 
 **Parameters:**
@@ -18,6 +62,9 @@ Converts a provided collection of individuals into a list of JSON descriptions.
 * ``ic::IndividualCollection``, the collection of individuals.
 * ``fs::FitnessScheme``, the fitness scheme used by the individuals belonging
   to the collection.
+* ``prefix::UTF8String``, the prefix that should be added to each JSON
+  description. Used to provide generation and deme numbers. If any information
+  is provided in the prefix, it MUST be suffixed by a comma.
 
 **Returns:**
 
@@ -25,18 +72,18 @@ A list of JSON descriptions, implemented as a vector of UTF-8 strings.
 """
 
 # Idea: Could supply a JSON prefix? (generations, deme number, etc).
-
-function individuals_to_log_entries(
+function individuals_to_json(
   ic::IndividualCollection,
-  fs::FitnessScheme
+  fs::FitnessScheme,
+  prefix::UTF8String
 )
   # Find the number of individuals in the provided collection.
   num = length(ic)
 
   # Create an array to hold the JSON objects for each of the individuals within
-  # the provided collection, initialised with the current generation number and
-  # the individual's position.
-  serialised = UTF8String["\"generation\": $(gen), \"position\": $(i)" for i in 1:num]
+  # the provided collection, initialised with the supplied prefix and the
+  # individual's position.
+  serialised = UTF8String["$(prefix) \"position\": $(i)" for i in 1:num]
 
   # Add each of the representations used by the individual to the log entries.
   for rep in keys(ic.stages)
@@ -45,9 +92,6 @@ function individuals_to_log_entries(
     end
   end
 
-  # Add the evaluated flag to each individual.
-  map!(json -> json * ", \"evaluated\": true", serialised)
-
   # Add the fitness values for each individual to the JSON objects, along with
   # their evaluated flag.
   for (i, json) in enumerate(serialised)
@@ -55,18 +99,8 @@ function individuals_to_log_entries(
   end
 
   # Add the opening and closing tags to each JSON object.
-  map!(json -> "{" * json * "}")
+  map!(json -> "{" * json * "}", serialised)
 
   # Return the vector of JSON descriptions.
-  return json
-end
-
-function write_entry_to_log_file(
-  l::EvoAnalyserLogger,
-  generation::Int,
-  position::Int,
-  deme::Int,
-  fitness::F
-)
-
+  return serialised
 end
