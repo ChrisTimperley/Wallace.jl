21273ce8822c13ac66e3401d095129080963b5e6
ChrisTimperley <christimperley@googlemail.com>
2015-12-07 15:05:04 +0000

    Modified TSP example.

--- src/example/tsp.jl
+++ src/example/tsp.jl
@@ -2,4 +2,26 @@
 Description of TSP, and the setup used to solve it.
 """
 function tsp()
+  # Compute the absolute location of the TSP example file.
+  file_name = "$(dirname(@__FILE__))/tsp/berlin-52.tsp"
+
+  def = algorithm.genetic() do alg
+    alg.population = population.simple() do pop
+      pop.size = 100
+      pop.species = species.simple() do sp
+        sp.fitness = fitness.scalar(false, Float)
+        sp.representation = representation.permutation(1:52)
+      end
+      pop.breeder = breeder.flat() do br
+        br.selection = selection.tournament(2)
+        br.mutation = mutation.single_swap()
+        br.crossover = crossover.null()
+      end
+    end
+    alg.evaluator = evaluator.tsp(file_name; threads = 8)
+    alg.termination["generations"] = criterion.generations(1000)
+  end
+
+  # Build and return the algorithm; somewhat redundant.
+  algorithm.compose!(def)
 end
