59c8e76d014d162a6c7c8c374d5127ec79b160dd
ChrisTimperley <christimperley@googlemail.com>
2015-12-07 13:31:39 +0000

    Modified mutation interface.

--- src/mutation.jl
+++ src/mutation.jl
@@ -4,7 +4,7 @@ TODO: Document mutation module.
 module mutation
 importall common, variation
 using core, utility, representation, individual, species
-export Mutation, MutationDefinition
+export Mutation, MutationDefinition, mutate!
 
 """
 The base type used by all mutation operation definitions.
@@ -16,9 +16,17 @@ The base type for all mutation operations.
 """
 abstract Mutation <: Variation
 
+"""
+Subjects the first `n` individuals from a provided collection to mutation,
+performed using a supplied mutation operator.
+"""
 operate!(m::Mutation, srcs::IndividualCollection, n::Int) =
   operate!(m, srcs.stages[m.stage][1:n])
 
+"""
+Subjects a list of chromosomes to mutation, performed using a supplied mutation
+operator.
+"""
 operate!{T}(m::Mutation, srcs::Vector{IndividualStage{T}}) =
   for src in srcs; operate!(m, src); end
 
@@ -27,7 +35,17 @@ Performs the mutation operation on the provided individual stage provided that
 it is in a valid state.
 """
 operate!{T}(m::Mutation, src::IndividualStage{T}) =
-  if valid(src); set(src, operate!(m, get(src))); end
+  if valid(src); set(src, mutate!(m, get(src))); end
+
+"""
+All mutation operators accept a single individual.
+"""
+num_inputs(m::Mutation) = 1
+
+"""
+All mutation operators produce a single individual.
+"""
+num_outputs(m::Mutation) = 1
 
 # Load all mutation operations.
 include("mutation/bit_flip.jl")
